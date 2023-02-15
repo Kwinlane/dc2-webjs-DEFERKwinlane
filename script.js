0f79b1e913669502a21d4f3b98eb8562cef60f2b
Kwinlane Defer <kwinlanedefer66@gmail.com>
2023-02-15 11:22:10 +0100

    essai du replaceChild

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ elt.innerHTML = "<div><li>Division1</li></div><div><li>Division2</li></div><div>
 const newElt = document.createElement('div');
 newElt.innerHTML = "<div><li>Division4</li></div><div><li>Division5</li></div><div><li>Division6</li></div>"
 elt.appendChild(newElt);
+
+
+const newSec = document.createElement('section');
+newSec.innerHTML = "<span><li>span1</li></span>";
+elt.replaceChild(document.createElement("section"), newElt);
