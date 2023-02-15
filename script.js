125e4c98cd9c9da3cc6941bd5db3fc6b0d8499d8
Kwinlane Defer <kwinlanedefer66@gmail.com>
2023-02-15 11:19:09 +0100

    ajout de div avec le js

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 let elt = document.getElementById('main');
 elt.innerHTML = "<div><li>Division1</li></div><div><li>Division2</li></div><div><li>Division3</li></div>" ;
+
+const newElt = document.createElement('div');
+newElt.innerHTML = "<div><li>Division4</li></div><div><li>Division5</li></div><div><li>Division6</li></div>"
+elt.appendChild(newElt);
